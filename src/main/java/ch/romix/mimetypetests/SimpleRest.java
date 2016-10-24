2a391060187949de0c553a16a750cf1c1adcbfdc
Roman Schaller <roman.schaller@gmail.com>
2016-10-24 16:57:41 +0200

    add a simple resource

---
+++ src/main/java/ch/romix/mimetypetests/SimpleRest.java
@@ -0,0 +1,17 @@
+package ch.romix.mimetypetests;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+public class SimpleRest {
+
+    @GET
+    @Path("/simple")
+    public Response getResource(@HeaderParam("user-agent") String acceptHeader) {
+        return Response.ok("Hello world!", "text/plain").build();
+    }
+
+}
