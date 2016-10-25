ebd92ca7c30b673013effd288dc68decfc966724
Roman Schaller <roman.schaller@gmail.com>
2016-10-25 10:45:07 +0200

    response with csv, pdf and xls

--- src/main/java/ch/romix/mimetypetests/ServiceImpl.java
+++ src/main/java/ch/romix/mimetypetests/ServiceImpl.java
@@ -1,11 +1,13 @@
 package ch.romix.mimetypetests;
 
 
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.apache.commons.io.IOUtils;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.*;
+import java.net.URL;
 
 /**
  * Created by sx9 on 10/24/16.
@@ -14,8 +16,35 @@ import javax.ws.rs.core.Response;
 public class ServiceImpl {
 
     @GET
-    @Produces({"application/resourcename-v1+pdf", "application/resourcename-v1+x-java-serialized-object", "application/resourcename-v1+xml", "application/resourcename-v1+json" })
+    @Produces({"application/resourcename-v1+pdf", "application/resourcename-v1+x-java-serialized-object", "application/resourcename-v1+xml", "application/resourcename-v1+json", "application/resourcename-v1+xls", "application/resourcename-v1+csv" })
     public Response getResource(@HeaderParam("Accept") String acceptHeader) {
+        if (acceptHeader.endsWith("pdf")) {
+            URL resource = Thread.currentThread().getContextClassLoader().getResource("sample.pdf");
+            StreamingOutput output = new MyStreamingOutput(resource);
+            return Response.ok().type(acceptHeader).entity(output).build();
+        } else if (acceptHeader.endsWith("csv")) {
+            URL resource = Thread.currentThread().getContextClassLoader().getResource("sample.csv");
+            StreamingOutput output = new MyStreamingOutput(resource);
+            return Response.ok().type(acceptHeader).entity(output).build();
+        } else if (acceptHeader.endsWith("xls")) {
+            URL resource = Thread.currentThread().getContextClassLoader().getResource("sample.xls");
+            StreamingOutput output = new MyStreamingOutput(resource);
+            return Response.ok().type(acceptHeader).entity(output).build();
+        }
         return Response.ok().type(acceptHeader).entity("produce " + acceptHeader).build();
     }
+    private class MyStreamingOutput implements StreamingOutput {
+        private URL url;
+
+        public MyStreamingOutput(URL url) {
+            this.url = url;
+        }
+
+        public void write(OutputStream os) throws IOException, WebApplicationException {
+            try (InputStream is = url.openStream()) {
+                IOUtils.copy(is, os);
+            }
+        }
+    }
 }
+
