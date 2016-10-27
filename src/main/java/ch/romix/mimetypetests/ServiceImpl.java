64a58a429872dd00da86c91c10fdc803caa5ea58
Roman Schaller <roman.schaller@gmail.com>
2016-10-27 10:28:46 +0200

    disable cacheing

--- src/main/java/ch/romix/mimetypetests/ServiceImpl.java
+++ src/main/java/ch/romix/mimetypetests/ServiceImpl.java
@@ -2,6 +2,7 @@ package ch.romix.mimetypetests;
 
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 import java.net.URL;
@@ -15,20 +16,23 @@ public class ServiceImpl {
     @GET
     @Produces({"application/resourcename-v1+pdf", "application/resourcename-v1+x-java-serialized-object", "application/resourcename-v1+xml", "application/resourcename-v1+json", "application/resourcename-v1+msexcel", "application/resourcename-v1+csv" })
     public Response getResource(@HeaderParam("Accept") String acceptHeader) {
+        StreamingOutput output;
         if (acceptHeader.endsWith("pdf")) {
             URL resource = Thread.currentThread().getContextClassLoader().getResource("sample.pdf");
-            StreamingOutput output = new MyStreamingOutput(resource);
-            return Response.ok().type(acceptHeader).entity(output).build();
+            output = new MyStreamingOutput(resource);
         } else if (acceptHeader.endsWith("csv")) {
             URL resource = Thread.currentThread().getContextClassLoader().getResource("sample.csv");
-            StreamingOutput output = new MyStreamingOutput(resource);
-            return Response.ok().type(acceptHeader).entity(output).build();
+            output = new MyStreamingOutput(resource);
         } else if (acceptHeader.endsWith("msexcel")) {
             URL resource = Thread.currentThread().getContextClassLoader().getResource("sample.xls");
-            StreamingOutput output = new MyStreamingOutput(resource);
-            return Response.ok().type(acceptHeader).entity(output).build();
+            output = new MyStreamingOutput(resource);
+        } else {
+            URL resource = Thread.currentThread().getContextClassLoader().getResource("sample.txt");
+            output = new MyStreamingOutput(resource);
         }
-        return Response.ok().type(acceptHeader).entity("produce " + acceptHeader).build();
+        CacheControl cacheControl = new CacheControl();
+        cacheControl.setNoCache(true);
+        return Response.ok().type(acceptHeader).entity(output).cacheControl(cacheControl).build();
     }
 
 }
